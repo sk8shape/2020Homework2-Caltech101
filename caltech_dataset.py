310ef2f4d5bb849f91417f837f5500c7926f40c3
sk8shape <sk8shape@users.noreply.github.com>
2020-04-28 12:48:50 +0200

    Add files via upload

---
+++ caltech_dataset.py
@@ -0,0 +1,53 @@
+from torchvision.datasets import VisionDataset
+
+from PIL import Image
+
+import os
+import os.path
+import sys
+
+class data_elem:
+    __init__(self, image, label):
+        self.sample = image
+        self.lbl = label
+
+    def get_sample(self):
+        return self.sample
+    def get_label(self):
+        return self.lbl
+
+
+def pil_loader(path):
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        return img.convert('RGB')
+
+
+class Caltech(VisionDataset):
+
+    my_dataset = []
+    def __init__(self, root, split='train', transform=None, target_transform=None):
+        super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
+
+        self.split = split
+
+        split_path = self.split + ".txt"
+        with open(split_path, "r" ) as sp:
+            row = sp.read()
+            img = pil_loader(split_path+"/"+row)
+            item = data_elem(img,row.split("/")[0])
+            my_dataset.append(item)
+
+    def __getitem__(self, index):
+
+        image, label =   my_dataset[index].get_image(), my_dataset[index].get_label()
+
+        if self.transform is not None:
+            image = self.transform(image)
+
+        return image, label
+
+    def __len__(self):
+        length = len(my_dataset)
+        return length
