0a5fa2f4db87eece4c9860c60338f080513ce617
sk8shape <foschi.francesco97@gmail.com>
2020-04-28 13:27:51 +0200

    deleted comments

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -18,7 +18,6 @@ class data_elem:
 
 
 def pil_loader(path):
-    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
     with open(path, 'rb') as f:
         img = Image.open(f)
         return img.convert('RGB')
