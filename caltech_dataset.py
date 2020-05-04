f214b60f4a9cd6f3f16eef3ea4f42ee23600122b
sk8shape <foschi.francesco97@gmail.com>
2020-05-04 19:38:22 +0200

    a

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -53,7 +53,7 @@ class Caltech(VisionDataset):
         length = len(self.images_dataset)
         return length
 
-    def split_good(self, ratio):
+    def split(self, ratio):
 
         tmp_train = []
         tmp_val = []
